a0f068780caf141be865d168180db7a35f5e94e9
choiejae <ejae8319@gmail.com>
2022-10-28 01:18:21 +0900

    feat: prac Problem4

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,35 @@
 package onboarding;
 
+import static java.lang.Character.isLowerCase;
+import static java.lang.Character.isUpperCase;
+
 public class Problem4 {
+    private static final int UPPERCASE_NUMBER = 155;
+    private static final int LOWERCASE_NUMBER = 219;
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        if (isNotValidWord(word)) {
+            return word;
+        }
+        return modifyString(word);
+    }
+    private static boolean isNotValidWord(String word) {
+        return word.length() == 0 || 1000 < word.length();
+    }
+    private static String modifyString(String word) {
+        char[] resultWord = word.toCharArray();
+        for (int i = 0; i < resultWord.length; i++) {
+            resultWord[i] = modifychar(resultWord[i]);
+        }
+        return String.valueOf(resultWord);
+    }
+    private static char modifychar(char ch) {
+        if (isUpperCase(ch)) {
+            return (char)(UPPERCASE_NUMBER - ch);
+
+        }
+        else if (isLowerCase(ch)) {
+            return (char)(LOWERCASE_NUMBER - ch);
+        }
+        return (ch);
     }
 }
