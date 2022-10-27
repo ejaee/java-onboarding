bd314e752c086858f861f69b5cea24ba5fa6ca31
choiejae <ejae8319@gmail.com>
2022-10-28 01:18:13 +0900

    feat: prac Problem3

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,38 @@
 package onboarding;
 
 public class Problem3 {
+    private static final int NUMBER_THREE = 3;
+    private static final int NUMBER_SIX = 6;
+    private static final int NUMBER_NINE = 9;
+
     public static int solution(int number) {
-        int answer = 0;
-        return answer;
+        if (isNotValidNumber(number))
+            return 0;
+        return getClapNumber(number);
+    }
+    private static boolean isNotValidNumber(int number) {
+        return number < 1 || 10000 < number;
+    }
+    private static int getClapNumber(int number) {
+        int clapNumber = 0;
+
+        for (int i = 1; i <= number; i++) {
+             clapNumber += countClap(i);
+        }
+        return clapNumber;
+    }
+    private static int countClap(int number) {
+        int clapNumber = 0;
+
+        while (number != 0) {
+            if (isClapNumber(number % 10)) {
+                ++clapNumber;
+            }
+            number /= 10;
+        }
+        return clapNumber;
+    }
+    private static boolean isClapNumber(int number) {
+        return number == NUMBER_THREE || number == NUMBER_SIX || number == NUMBER_NINE;
     }
 }
