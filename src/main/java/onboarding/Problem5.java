1d0b0432fe799f4b56b940d8f375d2536b0ec614
choiejae <ejae8319@gmail.com>
2022-10-28 18:42:34 +0900

    docs: solved 5

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,20 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-
 public class Problem5 {
+//    if (!isNotValidMoney(money)) {
+//        return money;
+//    }
+
+    private static final List<Integer> MONETARY_UNITS = List.of(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1);
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = new ArrayList<>();
+        for (int monetaryUnit : MONETARY_UNITS) {
+            answer.add(money / monetaryUnit);
+            money %= monetaryUnit;
+        }
         return answer;
     }
 }
