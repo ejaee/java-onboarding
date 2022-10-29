d5523d851c05e29a3246c7664643bee6e70434ad
choiejae <ejae8319@gmail.com>
2022-10-30 02:02:06 +0900

    refactor: 매서드 기능 분리 (validityChecker, getAnswer)

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -4,12 +4,24 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 public class Problem5 {
-//    if (!isNotValidMoney(money)) {
-//        return money;
-//    }
-
     private static final List<Integer> MONETARY_UNITS = List.of(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1);
+    private static final int MIN_NUMBER = 1;
+    private static final int MAX_NUMBER = 1000000;
     public static List<Integer> solution(int money) {
+        validityChecker(money);
+
+        return getAnswer(money);
+    }
+
+    private static void validityChecker(int money) {
+        if (!isValidateMoney(money)) {
+            throw new IllegalArgumentException("Not valid money");
+        }
+    }
+    private static boolean isValidateMoney(int money) {
+        return MIN_NUMBER <= money && money <= MAX_NUMBER;
+    }
+    private static List<Integer> getAnswer(int money) {
         List<Integer> answer = new ArrayList<>();
         for (int monetaryUnit : MONETARY_UNITS) {
             answer.add(money / monetaryUnit);
